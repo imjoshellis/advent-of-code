bd480c503167cb540362233b5595fc73fe6be56b
Josh Ellis <josh@imjoshellis.com>
2020-12-03 08:25:58 -0600

    Clean up, inspired by callimx

--- 2020/day03/day03.go
+++ 2020/day03/day03.go
@@ -1,56 +1,35 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"strings"
 )
 
+func count(m []string, s []int) int {
+	c := 0
+	for x, y := 0, 0; y < len(m); x, y = x+s[0], y+s[1] {
+		x = x % len(m[0])
+		if m[y][x] == '#' {
+			c++
+		}
+	}
+	return c
+}
+
 func main() {
-	file, err := os.Open("./input.txt")
+	file, err := ioutil.ReadFile("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
 
-	lines := []string{}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
-	}
+	lines := strings.Split(string(file), "\n")
 
-	count := make([]int, 5)
-	idx := 0
-	for k := 1; k < 8; k += 2 {
-		x := 0
-		for _, line := range lines {
-			x = x % len(line)
-
-			if line[x] == '#' {
-				count[idx]++
-			}
-			x += k
-		}
-		idx++
-	}
-
-	x := 0
-	for i, line := range lines {
-		if i%2 != 0 {
-			continue
-		}
-		x = x % len(line)
-
-		if line[x] == '#' {
-			count[idx]++
-		}
-		x += 1
-	}
+	slopes := [][]int{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}}
 	ans := 1
-	for _, c := range count {
-		ans *= c
+	for _, slope := range slopes {
+		ans *= count(lines, slope)
 	}
 
 	fmt.Println(ans)
