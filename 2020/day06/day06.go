457fd55193628e19d4a99ac84b67ed53a6bd8808
Josh Ellis <josh@imjoshellis.com>
2020-12-06 14:23:35 -0600

    Solve day 6

--- 2020/day06/day06.go
+++ 2020/day06/day06.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
 func main() {
+	f, _ := ioutil.ReadFile("./input.txt")
+	gs := strings.Split(string(f), "\n")
+
+	count := 0
+	for i := 0; i < len(gs); i++ {
+		ltrs := make([]int, 26)
+		l := 0
+		for len(gs[i]) > 0 {
+			for _, ans := range gs[i] {
+				ltrs[ans-'a']++
+			}
+			l++
+			i++
+		}
+		for _, ltr := range ltrs {
+			if ltr == l {
+				count++
+			}
+		}
+	}
+	fmt.Println(count)
 }
