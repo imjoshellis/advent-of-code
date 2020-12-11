34db4a8906ed8dbca91203cc5667bccd445476e5
Josh Ellis <josh@imjoshellis.com>
2020-12-10 21:51:06 -0600

    Solve day 9 and 10

--- 2020/day09/day09.go
+++ 2020/day09/day09.go
@@ -1,4 +1,73 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	f, _ := ioutil.ReadFile("./input.txt")
+	lines := strings.Split(string(f), "\n")
+	nums := make([]int, len(lines))
+	for i := range lines {
+		nums[i], _ = strconv.Atoi(lines[i])
+	}
+
+	// part 1
+	// MainLoop:
+	// 	for i := 0; i < len(lines)-25; i++ {
+	// 		target, _ := strconv.Atoi(lines[i+25])
+	// 		for j := i; j < i+25; j++ {
+	// 			for k := j + 1; k < i+25; k++ {
+	// 				n1, _ := strconv.Atoi(lines[j])
+	// 				n2, _ := strconv.Atoi(lines[k])
+	// 				if n1+n2 == target {
+	// 					continue MainLoop
+	// 				}
+	// 			}
+	// 		}
+	// 		fmt.Println(i, target)
+	// 		return
+	// 	}
+
+	for start := 0; start < len(nums)-1; start++ {
+		for end := start + 1; sum(nums[start:end+1]) <= 1398413738; end++ {
+			if a := nums[start : end+1]; sum(a) == 1398413738 {
+				fmt.Println(start, end, min(a)+max(a))
+				return
+			}
+		}
+	}
+	fmt.Println("not found")
+}
+
+func sum(arr []int) int {
+	result := 0
+	for _, v := range arr {
+		result += v
+	}
+	return result
+}
+
+func min(arr []int) int {
+	res := math.MaxInt32
+	for _, v := range arr {
+		if v < res {
+			res = v
+		}
+	}
+	return res
+}
+
+func max(arr []int) int {
+	res := arr[0]
+	for _, v := range arr {
+		if v > res {
+			res = v
+		}
+	}
+	return res
 }
--- 2020/day10/day10.go
+++ 2020/day10/day10.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	f, _ := ioutil.ReadFile("./input.txt")
+	lines := strings.Split(string(f), "\n")
+	nums := make([]int, len(lines))
+	m := make(map[int]bool)
+	for i := range lines {
+		nums[i], _ = strconv.Atoi(lines[i])
+		m[nums[i]] = true
+	}
+	sort.Ints(nums)
+	// nums = append(nums, nums[len(nums)-1]+3)
+	arr := make([]int, nums[len(nums)-1])
+	if m[1] {
+		arr[0] = 1
+	}
+	if m[2] {
+		arr[1] = arr[0] + 1
+	}
+	if m[3] {
+		arr[2] = arr[1] + arr[0] + 1
+	}
+	for i := 3; i < len(arr); i++ {
+		if m[i+1] {
+			arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+		}
+	}
+	fmt.Println(arr[len(arr)-1])
 }
