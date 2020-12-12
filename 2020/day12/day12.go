734d25a38868debfb072e86cec62622bfe5decf5
Josh Ellis <josh@imjoshellis.com>
2020-12-12 11:04:23 -0600

    Make code more readable

--- 2020/day12/day12.go
+++ 2020/day12/day12.go
@@ -18,32 +18,32 @@ func main() {
 	lines := strings.Split(string(f), "\n")
 
 	facing := 1
-	p := coordinate{0, 0}
-	w := coordinate{10, 1}
-	m := map[byte]int{'R': 1, 'L': -1, 'N': 1, 'S': -1, 'W': -1, 'E': 1}
+	ship := coordinate{0, 0}
+	waypoint := coordinate{10, 1}
+	dirMap := map[byte]int{'R': 1, 'L': -1, 'N': 1, 'S': -1, 'W': -1, 'E': 1}
 	for _, line := range lines {
 		dir := line[0]
-		n, err := strconv.Atoi(line[1:])
+		dist, err := strconv.Atoi(line[1:])
 		if err != nil {
 			panic(err)
 		}
 
 		switch dir {
-		case 'F':
-			p.x += w.x * n
-			p.y += w.y * n
 		case 'N', 'S':
-			w.y += n * m[dir]
+			waypoint.y += dist * dirMap[dir]
 		case 'W', 'E':
-			w.x += n * m[dir]
+			waypoint.x += dist * dirMap[dir]
 		case 'R', 'L':
 			pre := facing + 4
-			facing = (pre + n/90*m[dir]) % 4
+			facing = (pre + dist/90*dirMap[dir]) % 4
 			for i := 0; i < (pre-facing)%4; i++ {
-				w.x, w.y = -w.y, w.x
+				waypoint.x, waypoint.y = -waypoint.y, waypoint.x
 			}
+		case 'F':
+			ship.x += waypoint.x * dist
+			ship.y += waypoint.y * dist
 		}
 	}
 
-	fmt.Println(math.Abs(float64(p.x)) + math.Abs(float64(p.y)))
+	fmt.Println(math.Abs(float64(ship.x)) + math.Abs(float64(ship.y)))
 }
