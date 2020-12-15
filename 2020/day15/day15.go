99b853c97e909b79fb59b8aa32e851a454be0f55
Josh Ellis <josh@imjoshellis.com>
2020-12-15 08:13:47 -0600

    Solve day 15

--- 2020/day15/day15.go
+++ 2020/day15/day15.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	ns := []int{0, 12, 6, 13, 20, 1}
+	m := make(map[int]int)
+	for i, n := range ns {
+		m[n] = i
+	}
+
+	nxt := 17
+	for i := 6; i < 30000000-1; i++ {
+		if idx, ok := m[nxt]; ok {
+			nxt, m[nxt] = i-idx, i
+		} else {
+			m[nxt], nxt = i, 0
+		}
+	}
+
+	fmt.Println(nxt)
 }
