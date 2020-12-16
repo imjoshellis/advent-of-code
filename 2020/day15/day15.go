ca6e0aadf941b54281b7ba6f3d32cfcc5e4f5e23
Josh Ellis <josh@imjoshellis.com>
2020-12-16 07:57:26 -0600

    Clean up code

--- 2020/day15/day15.go
+++ 2020/day15/day15.go
@@ -2,26 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	t := time.Now()
 	initial := []int{0, 12, 6, 13, 20, 1, 17}
 	target := 30000000
 	fmt.Println(getNth(initial, target))
-	fmt.Println(time.Since(t))
 }
 
 func getNth(initial []int, target int) int {
 	l := len(initial) - 1
-	next, ns := initial[l], initial[:l]
+	next := initial[l]
+
 	m := make(map[int]int)
 	for i, n := range initial {
 		m[n] = i
 	}
 
-	for i := len(ns); i < target-1; i++ {
+	for i := l; i < target-1; i++ {
 		if prev, ok := m[next]; ok {
 			next, m[next] = i-prev, i
 		} else {
